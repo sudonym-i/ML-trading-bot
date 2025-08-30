79935e92955af894a766b0a0952ed8bd72420dc3
sudonym-i <isickln08@gmail.com>
2025-08-30 12:29:48 -0600

    need to debug tsr_model test script. filepath error

--- src/tsr_model/__init__.py
+++ src/tsr_model/__init__.py
@@ -1,4 +1,7 @@
 # src package init
 from .route import train_model, test_run_model
+from .model import GRUPredictor
+from .data_pipeline import DataLoader
+from .utils import add_technical_indicators, create_sequences
 
 # just need to add "use_model" functionality
